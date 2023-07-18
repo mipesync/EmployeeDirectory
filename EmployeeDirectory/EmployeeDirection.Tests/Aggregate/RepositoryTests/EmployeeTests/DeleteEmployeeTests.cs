aa29c81503d64c28fc7b99b4383ab6dbe2cf89b1
Alferov Arkady <arhe414@gmail.com>
2023-07-19 01:29:47 +0300

    Добавил тесты для удаления сотрудника

---
+++ EmployeeDirectory/EmployeeDirection.Tests/Aggregate/RepositoryTests/EmployeeTests/DeleteEmployeeTests.cs
@@ -0,0 +1,54 @@
+﻿using EmployeeDirection.Tests.Common;
+using EmployeeDirectory.Application.Exceptions;
+using EmployeeDirectory.Application.Repository;
+using System.Threading.Tasks;
+using System;
+using Xunit;
+
+namespace EmployeeDirection.Tests.Aggregate.RepositoryTests.EmployeeTests
+{
+    /// <summary>
+    /// Тест метода репозитория <see cref="EmployeeRepository"/> для удаления сотрудника
+    /// </summary>
+    public class DeleteEmployeeTests : TestCommandBase
+    {
+        private EmployeeRepository employeeRepository;
+
+        public DeleteEmployeeTests()
+        {
+            employeeRepository = new EmployeeRepository(_dbContext);
+        }
+
+        /// <summary>
+        /// Проверяет успешное удаление пользователя
+        /// </summary>
+        [Fact]
+        public void Delete_Success()
+        {
+
+            //Arrange
+            var employeeId = TestDBContext.EmployeeId;
+
+            //Act
+            var result = employeeRepository.Delete(employeeId);
+
+            //Assert
+            Assert.IsType<Task>(result);
+            Assert.Throws<NotFoundException>(() => employeeRepository.GetById(employeeId));
+        }
+
+        /// <summary>
+        /// Проверяет выбрасывание исключения <see cref="NotFoundException"/>
+        /// </summary>
+        [Fact]
+        public async Task Delete_FailOnNotFoundAsync()
+        {
+            //Arrange
+            var employeeId = Guid.NewGuid();
+
+            //Act
+            //Assert
+            await Assert.ThrowsAsync<NotFoundException>(() => employeeRepository.Delete(employeeId));
+        }
+    }
+}
---
+++ EmployeeDirectory/EmployeeDirection.Tests/Aggregate/RepositoryTests/EmployeeTests/GetByIdEmployeeTests.cs
@@ -0,0 +1,52 @@
+﻿using EmployeeDirection.Tests.Common;
+using EmployeeDirectory.Application.Exceptions;
+using EmployeeDirectory.Application.Repository;
+using EmployeeDirectory.Domain;
+using System;
+using Xunit;
+
+namespace EmployeeDirection.Tests.Aggregate.RepositoryTests.EmployeeTests
+{
+    /// <summary>
+    /// Тест метода репозитория <see cref="EmployeeRepository"/> для 
+    /// получения информации о сотруднике
+    /// </summary>
+    public class GetByIdEmployeeTests : TestCommandBase
+    {
+        private EmployeeRepository employeeRepository;
+
+        public GetByIdEmployeeTests()
+        {
+            employeeRepository = new EmployeeRepository(_dbContext);
+        }
+
+        /// <summary>
+        /// Проверяет успешный вывод информации о сотруднике+
+        /// </summary>
+        [Fact]
+        public void GetById_Success()
+        {
+            //Arrange
+            var employeeId = TestDBContext.EmployeeId;
+
+            //Act
+            var result = employeeRepository.GetById(employeeId);
+
+            //Assert
+            Assert.IsType<Employee>(result);
+        }
+
+        /// <summary>
+        /// Проверяет выбрасывание исключения <see cref="NotFoundException"/>
+        /// </summary>
+        [Fact]
+        public void GetById_FailOnNotFoundAsync()
+        {
+            //Arrange
+            var employeeId = Guid.NewGuid();
+            //Act
+            //Assert
+            Assert.Throws<NotFoundException>(() => employeeRepository.GetById(employeeId));
+        }
+    }
+}
